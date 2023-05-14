d1612940b499c3a6138d77527c04c2b232eeeeb4
242385 <126722242+242385@users.noreply.github.com>
2023-05-14 15:23:35 +0200

    Another approach to AbstractModelAPI

--- Model/AbstractModelAPI.cs
+++ Model/AbstractModelAPI.cs
@@ -1,42 +1,82 @@
 ﻿using Logika;
-using System.Collections.ObjectModel;
 
 namespace Model
 {
-    public abstract class ModelApi
+    public abstract class AbstractModelAPI : IObserver<int>
     {
-       private static ModelApi _apiInstance = new Model();
-
-       public static ModelApi ApiInstance
+        public static AbstractModelAPI CreateNewInstance()
         {
-            get { return _apiInstance; }
+            return new ModelAPI();
         }
+        public abstract void BallsToModelBalls(int num);
 
-        public abstract void ConvertBallsToModelBalls();
+        public abstract void ClearModelBoard();
+        public abstract void StartModelBalls();
+        public abstract IModelBall GetModelBall(int i);
+        public abstract void OnCompleted();
+        public abstract void OnError(Exception error);
+        public abstract void OnNext(int i);
 
-        public abstract ObservableCollection<IModelBall> GetModelBallCollection();
-
-        internal sealed class Model : ModelApi
+        internal sealed class ModelAPI : AbstractModelAPI
         {
-            private LogicApi logicApi = LogicApi.ApiInstance;
+            private readonly AbstractLogicAPI logicApi;           
+            private readonly List<IModelBall> ModelBallList = new List<IModelBall>();
+            private readonly IDisposable? observerManager;
 
-            ObservableCollection<IModelBall> ModelBallCollection = new ObservableCollection<IModelBall>();
-
-            public override void ConvertBallsToModelBalls()         
+            internal ModelAPI()
             {
-                List<IBall> ballList = logicApi.GetBallList(); 
-                ModelBallCollection.Clear();                 
-                foreach (IBall ball in ballList)
+                logicApi = AbstractLogicAPI.CreateNewInstance();
+                observerManager = logicApi.Subscribe(this);
+            }
+
+            public override void ClearModelBoard()
+            {
+                logicApi.ClearBoard();
+                ModelBallList.Clear();
+                observerManager?.Dispose();
+            }
+
+            public override void BallsToModelBalls(int num)
+            {
+                logicApi.CreateBoard();
+                logicApi.CreateBalls(num);
+                List<List<double>> dataBalls = logicApi.GetBallsCoordsAndRadius();
+                for (int i = 0; i < dataBalls.Count; i++)
                 {
-                    IModelBall newModelBall = IModelBall.CreateModelBall(ball);
-                    ModelBallCollection.Add(newModelBall);
+                    List<double> ballCoords = dataBalls[i];
+                    ModelBallList.Add(IModelBall.CreateModelBall(ballCoords[0], ballCoords[1], ballCoords[2]));
                 }
             }
 
-            public override ObservableCollection<IModelBall> GetModelBallCollection()
+            public override IModelBall GetModelBall(int i)
             {
-                return ModelBallCollection;
+                return ModelBallList[i];
             }
+
+            public override void OnCompleted()
+            {
+                observerManager?.Dispose();
+            }
+
+            public override void OnError(Exception error)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void OnNext(int i)
+            {
+                if (i < ModelBallList.Count)
+                {
+                    IModelBall modelBall = ModelBallList[i];
+                    List<double> modelBallCoords = logicApi.GetBallsCoordsAndRadius()[i];
+                    modelBall.Moving(modelBallCoords[0], modelBallCoords[1]);
+                }
+            }
+
+            public override void StartModelBalls()
+            {
+                logicApi.StartBalls();
+            }           
         }
     }
 }
